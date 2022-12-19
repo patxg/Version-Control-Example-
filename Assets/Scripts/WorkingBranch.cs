f234dbdddc34c071770b5965551bd8a482cff913
Pat G <pat.x.guillen@gmail.com>
2022-12-19 17:17:19 -0500

    dev script removed from inv branch

--- Assets/Scripts/WorkingBranch.cs
+++ Assets/Scripts/WorkingBranch.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DevBranch : MonoBehaviour
+public class WorkingBranch : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
